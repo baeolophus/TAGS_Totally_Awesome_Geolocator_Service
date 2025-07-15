3b1e1f818da70bf7ed426e7084b340afc9bb736a
Claire Curry (OU laptop) <cmcurry@ou.edu>
2025-07-15 22:51:07 +0000

    trying to get leaflet or mapview to work.  mapview seems more promising though will require a bit of shenanigans to integrate into R Shiny.

--- src/testing_get_elevation_bugs.R
+++ src/testing_get_elevation_bugs.R
@@ -42,6 +42,9 @@ coord <- GeoLight::coord(tFirst = twl_rise$Twilight, # formerly named tFirst, le
                          tSecond = twl_rise$tSecond,
                          type = twl_rise$type,
                          degElevation=elev[[2]])
+coord.df <- data.frame(coord)
+coord.df$lng <- coord.df$lon #rename default to match what addMarkers in leaflet() lines below requires.
+coord.df$lon <- NULL #delete old column
 
 library(TwGeos)
 library(SGAT)
@@ -49,3 +52,41 @@ library(GeoLight)
 library(MASS)
 
 calib <- TwGeos::thresholdCalibration(calib$Twilight, calib$Rise, lon.calib, lat.calib, method = "log-norm")
+
+library(leaflet)
+#run the leaflet function
+m <- leaflet() %>%
+  addProviderTiles(
+    "Stamen.Toner",
+    group = "Stamen.Toner"
+  )
+
+m
+
+  
+  #add the calculated coordinates based on edited twilights
+map_1 <- m %>%
+  addAwesomeMarkers(
+    lat = 48.1,
+    lng = 11.5,
+    label = "Starting point"
+  )
+
+library(mapview)
+#https://bookdown.org/nicohahn/making_maps_with_r5/docs/mapview.html
+
+library(sf)
+
+#https://tmieno2.github.io/R-as-GIS-for-Economists/turning-a-data-frame-of-points-into-an-sf.html
+coord.sf <- st_as_sf(na.omit(coord.df), coords = c("lng", "lat"), crs = 4326)
+
+#possibly not rendering in leaflet because of number of markers: https://stackoverflow.com/questions/34607908/using-many-markers-with-leaflet-in-combination-with-shiny-server 
+#suggests mapview as an alternative or clustering (which is probably not good for this use case?)
+#or this: https://github.com/r-spatial/leafgl
+
+mapview(coord.sf, legend = FALSE)
+
+#https://stackoverflow.com/questions/65485747/mapview-points-not-showing-in-r
+
+#https://stackoverflow.com/questions/36679944/mapview-for-shiny#36682268
+#https://www.spsanderson.com/steveondata/posts/rtip-2023-05-04/index.html
